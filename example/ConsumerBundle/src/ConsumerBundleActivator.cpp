f79be40a42e01f009add7f90d61b175a6a762aa0
Andreas Stahl <andreas.bjorn.stahl@gmail.com>
2013-03-08 11:01:53 +0100

    Tweaks to consumer example and exports

--- example/ConsumerBundle/src/ConsumerBundleActivator.cpp
+++ example/ConsumerBundle/src/ConsumerBundleActivator.cpp
@@ -2,12 +2,13 @@
 #include "Poco/OSP/ServiceRegistry.h"
 #include "Poco/Logger.h"
 #include "AdditionService.h"
+#include "Poco/NumberFormatter.h"
 
 void ConsumerBundleActivator::start(Poco::OSP::BundleContext::Ptr context)
 {
 	Poco::AutoPtr<AdditionService> service = context->registry().findByName("com.example.additionservice")->castedInstance<AdditionService>();
 	int result = service->add(3, 4);
-	context->logger().information("result "+result);
+	context->logger().information("Addition Service says: 3 + 4 = "+ Poco::NumberFormatter().format(result));
 }
 
 void ConsumerBundleActivator::stop(Poco::OSP::BundleContext::Ptr context)
